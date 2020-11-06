cfcc8b258de3e58a5576e39a4d0d0efa322dd2c6
杨凤玲 <yangfengling@inttech.cn>
2020-11-06 22:53:29 +0800

    剑指 Offer 53 - I. 在排序数组中查找数字 I

--- 2020-11/剑指 Offer 53 - I. 在排序数组中查找数字 I.py
+++ 2020-11/剑指 Offer 53 - I. 在排序数组中查找数字 I.py
@@ -15,6 +15,44 @@
 0 <= 数组长度 <= 50000
 '''
 
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: [int], target: int) -> int:
         return nums.count(target)
+
+
+'''
+# 别人的方法  二分查找
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        def helper(tar):
+            i, j = 0, len(nums) - 1
+            while i <= j:
+                m = (i + j) // 2
+                if nums[m] <= tar: i = m + 1
+                else: j = m - 1
+            return i
+        return helper(target) - helper(target - 1)
+
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        # 搜索右边界 right
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] <= target: i = m + 1
+            else: j = m - 1
+        right = i
+        # 若数组中无 target ，则提前返回
+        if j >= 0 and nums[j] != target: return 0
+        # 搜索左边界 left
+        i = 0
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] < target: i = m + 1
+            else: j = m - 1
+        left = j
+        return right - left - 1
+
+
+'''
