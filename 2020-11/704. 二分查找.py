8501959863f889423543f82a6f0422a443e7e4c4
杨凤玲 <yangfengling@inttech.cn>
2020-11-07 14:49:55 +0800

    704. 二分查找

--- 2020-11/704. 二分查找.py
+++ 2020-11/704. 二分查找.py
@@ -21,22 +21,37 @@ n 将在 [1, 10000]之间。
 nums 的每个元素都将在 [-9999, 9999]之间。
 
 '''
+# 下面是本人的代码：
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = left + (right - left) // 2
+            if nums[middle] == target:
+                return middle
+            if nums[middle] < target:
+                left = middle+1
+            else:
+                right = middle-1
+        return -1
 
+''''
+# 别人的代码
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0;
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right-left) // 2
+            if target == nums[mid]: return mid
+            if target < nums[mid]:
+                right = mid - 1
+            else :
+                left = mid + 1
+        return -1
 
-def search(nums, target):
-    left, right = 0, len(nums) - 1
+复杂度分析
 
-    while left <= right:
-        middle = left + (right - left) // 2
-        if nums[middle] == target:
-            return middle
-        if nums[middle] < target:
-            left = middle+1
-        else:
-            right = middle-1
-    return -1
-
-
-nums = [-1, 0, 3, 5, 9, 12]
-target = 12
-print(search(nums, target))
+时间复杂度: O(logn)
+空间复杂度: O(1)
+'''
