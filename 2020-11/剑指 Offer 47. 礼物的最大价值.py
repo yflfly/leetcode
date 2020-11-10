b67856cb3c6fa90e65068928e91ddc35cbf05e76
杨凤玲 <yangfengling@inttech.cn>
2020-11-10 22:46:39 +0800

    动态规划-剑指 Offer 47. 礼物的最大价值

--- 2020-11/剑指 Offer 47. 礼物的最大价值.py
+++ 2020-11/剑指 Offer 47. 礼物的最大价值.py
@@ -20,6 +20,7 @@
 
 '''
 
+
 class Solution:
     def maxValue(self, grid: List[List[int]]) -> int:
         for i in range(len(grid)):
@@ -29,7 +30,7 @@ class Solution:
                 if i == 0:
                     grid[i][j] += grid[i][j - 1]
                 elif j == 0:
-                    grid[i][j] += grid[i-1][j]
+                    grid[i][j] += grid[i - 1][j]
                 else:
                     grid[i][j] += max(grid[i - 1][j], grid[i][j - 1])
         return grid[-1][-1]
