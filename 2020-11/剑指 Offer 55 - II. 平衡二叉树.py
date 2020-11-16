1e44c6d96bed61cd2ee7367f7a11cb1a07338ccf
杨凤玲 <yangfengling@inttech.cn>
2020-11-16 19:26:32 +0800

    树-剑指 Offer 55 - II. 平衡二叉树

---
+++ 2020-11/剑指 Offer 55 - II. 平衡二叉树.py
@@ -0,0 +1,51 @@
+# coding:utf-8
+'''
+剑指 Offer 55 - II. 平衡二叉树
+输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+
+示例 1:
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+
+限制：
+1 <= 树的结点个数 <= 10000
+注意：本题与主站 110 题相同：https://leetcode-cn.com/problems/balanced-binary-tree/
+
+'''
+
+
+# 方法一：后序遍历+剪枝（从底至顶）
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def recur(root):
+            if not root: return 0
+            left = recur(root.left)
+            if left == -1: return -1
+            right = recur(root.right)
+            if right == -1: return -1
+            return max(left, right) + 1 if abs(left - right) <= 1 else -1
+
+        res_ = recur(root)
+        if res_ == -1:
+            return False
+        else:
+            return True
