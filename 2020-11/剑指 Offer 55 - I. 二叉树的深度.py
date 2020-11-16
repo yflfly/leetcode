6146bf24656ef45a914ec41158226395a77379be
杨凤玲 <yangfengling@inttech.cn>
2020-11-16 19:15:46 +0800

    树-剑指 Offer 55 - I. 二叉树的深度

---
+++ 2020-11/剑指 Offer 55 - I. 二叉树的深度.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+'''
+剑指 Offer 55 - I. 二叉树的深度
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7]，
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+提示：
+
+节点总数 <= 10000
+
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 方法一：后序遍历  DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# 方法二 层序遍历
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        queue = [root]
+        res = 0
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            queue = tmp
+            res += 1
+        return res
