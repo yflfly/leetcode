d11c559ea1c33667a69ef1a86dc9ae2ce0819118
杨凤玲 <yangfengling@inttech.cn>
2020-11-18 21:40:48 +0800

    树-剑指 Offer 32 - II. 从上到下打印二叉树 II

---
+++ 2020-11/剑指 Offer 32 - II. 从上到下打印二叉树 II.py
@@ -0,0 +1,49 @@
+# coding:utf-8
+'''
+剑指 Offer 32 - II. 从上到下打印二叉树 II
+从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+'''
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            tmp = []
+            ceng = []
+            for node in stack:
+                ceng.append(node.val)
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            stack = tmp
+            if ceng:
+                res.append(ceng)
+        return res
+
+
