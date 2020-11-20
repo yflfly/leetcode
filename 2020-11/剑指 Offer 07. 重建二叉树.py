020ec9e0bb90ca615618f3e9190da36ead7aec21
杨凤玲 <yangfengling@inttech.cn>
2020-11-20 11:11:03 +0800

    树-剑指 Offer 07. 重建二叉树

--- 2020-11/剑指 Offer 07. 重建二叉树.py
+++ 2020-11/剑指 Offer 07. 重建二叉树.py
@@ -15,10 +15,12 @@
    15   7
  
 限制：
-
+0
 0 <= 节点个数 <= 5000
 
 '''
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -26,12 +28,13 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if not preorder:
             return None
         loc = inorder.index(preorder[0])
         root = TreeNode(preorder[0])
-        root.left = self.buildTree(preorder[1:loc+1],inorder[:loc])
-        root.right = self.buildTree(preorder[loc+1:],inorder[loc+1:])
+        root.left = self.buildTree(preorder[1:loc + 1], inorder[:loc])
+        root.right = self.buildTree(preorder[loc + 1:], inorder[loc + 1:])
         return root
