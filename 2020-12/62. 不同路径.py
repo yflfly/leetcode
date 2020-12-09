7d4370c5803613f49008b1d49c2860c99a81be43
杨凤玲 <yangfengling@inttech.cn>
2020-12-09 21:59:05 +0800

    动态规划-62. 不同路径

---
+++ 2020-12/62. 不同路径.py
@@ -0,0 +1,47 @@
+# coding:utf-8
+'''
+62. 不同路径
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+
+示例 1：
+输入：m = 3, n = 7
+输出：28
+示例 2：
+
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 3：
+
+输入：m = 7, n = 3
+输出：28
+示例 4：
+
+输入：m = 3, n = 3
+输出：6
+ 
+
+提示：
+
+1 <= m, n <= 100
+题目数据保证答案小于等于 2 * 109
+
+'''
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for j in range(n)] for i in range(m)]
+        dp[0] = [1 for i in range(n)]
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[-1][-1]
