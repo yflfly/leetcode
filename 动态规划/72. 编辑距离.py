98c5b6b401a7844f0a1b869108a6262de05fa800
杨凤玲 <yangfengling@inttech.cn>
2020-12-11 18:34:50 +0800

    动态规划

--- 动态规划/72. 编辑距离.py
+++ 动态规划/72. 编辑距离.py
@@ -2,13 +2,10 @@
 '''
 72. 编辑距离
 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
-
 你可以对一个单词进行如下三种操作：
-
 插入一个字符
 删除一个字符
 替换一个字符
-
 示例 1：
 输入：word1 = "horse", word2 = "ros"
 输出：3
@@ -25,7 +22,6 @@ inention -> enention (将 'i' 替换为 'e')
 enention -> exention (将 'n' 替换为 'x')
 exention -> exection (将 'n' 替换为 'c')
 exection -> execution (插入 'u')
- 
 提示：
 0 <= word1.length, word2.length <= 500
 word1 和 word2 由小写英文字母组成
