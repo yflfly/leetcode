bdc2a1bbd5dc7b4bef074370182d07fdbd86d5de
杨凤玲 <yangfengling@inttech.cn>
2020-12-12 14:55:54 +0800

    排序-922. 按奇偶排序数组 II

---
+++ 2020-12/922. 按奇偶排序数组 II.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+'''
+922. 按奇偶排序数组 II
+给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+你可以返回任何满足上述条件的数组作为答案。
+示例：
+输入：[4,2,5,7]
+输出：[4,5,2,7]
+解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ 
+提示：
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+
+'''
+
+
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        ji, ou = [], []
+        for each in A:
+            if each % 2 == 0:
+                ou.append(each)
+            else:
+                ji.append(each)
+        ji.sort()
+        ou.sort()
+        res = []
+        for i in range(len(ji)):
+            res.append(ou[i])
+            res.append(ji[i])
+        return res
