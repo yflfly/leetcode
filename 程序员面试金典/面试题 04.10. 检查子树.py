7cd1f25fa42ac0c9e365f4ab3b46a7f4b49d16d0
杨凤玲 <yangfengling@inttech.cn>
2021-01-02 16:14:57 +0800

    面试题 04.10. 检查子树

---
+++ 程序员面试金典/面试题 04.10. 检查子树.py
@@ -0,0 +1,34 @@
+'''
+面试题 04.10. 检查子树
+检查子树。你有两棵非常大的二叉树：T1，有几万个节点；T2，有几万个节点。设计一个算法，判断 T2 是否为 T1 的子树。
+
+如果 T1 有这么一个节点 n，其子树与 T2 一模一样，则 T2 为 T1 的子树，也就是说，从节点 n 处把树砍断，得到的树与 T2 完全相同。
+
+示例1:
+
+ 输入：t1 = [1, 2, 3], t2 = [2]
+ 输出：true
+示例2:
+
+ 输入：t1 = [1, null, 2, 4], t2 = [3, 2]
+ 输出：false
+'''
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def checkSubTree(self, t1: TreeNode, t2: TreeNode) -> bool:
+        if not t1 or not t2:
+            return False
+        return self.check(t1, t2) or self.checkSubTree(t1.left, t2) or self.checkSubTree(t1.right, t2)
+    def check(self, s, t):  # 这里 t 可能为空
+        if not t:
+            return True
+        if not s:
+            return False
+
+        return s.val == t.val and self.check(s.left, t.left) and self.check(s.right, t.right)
