00b7ea476f192a6c8caede69e8f373c3c81d8acd
杨凤玲 <yangfengling@inttech.cn>
2021-01-03 19:27:58 +0800

    面试题 16.09. 运算

---
+++ 程序员面试金典/面试题 16.09. 运算.py
@@ -0,0 +1,42 @@
+'''
+面试题 16.09. 运算
+请实现整数数字的乘法、减法和除法运算，运算结果均为整数数字，程序中只允许使用加法运算符和逻辑运算符，允许程序中出现正负常数，不允许使用位运算。
+
+你的实现应该支持如下操作：
+
+Operations() 构造函数
+minus(a, b) 减法，返回a - b
+multiply(a, b) 乘法，返回a * b
+divide(a, b) 除法，返回a / b
+示例：
+
+Operations operations = new Operations();
+operations.minus(1, 2); //返回-1
+operations.multiply(3, 4); //返回12
+operations.divide(5, -2); //返回-2
+提示：
+
+你可以假设函数输入一定是有效的，例如不会出现除法分母为0的情况
+单个用例的函数调用次数不会超过1000次
+'''
+
+
+class Operations:
+
+    def __init__(self):
+        self.info = []
+
+    def minus(self, a: int, b: int) -> int:
+        return a - b
+
+    def multiply(self, a: int, b: int) -> int:
+        return a * b
+
+    def divide(self, a: int, b: int) -> int:
+        return int(a / b)
+
+# Your Operations object will be instantiated and called as such:
+# obj = Operations()
+# param_1 = obj.minus(a,b)
+# param_2 = obj.multiply(a,b)
+# param_3 = obj.divide(a,b)
