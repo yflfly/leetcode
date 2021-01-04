74fcd5c98f821a68fbf08dc5bb385cef553b59b8
杨凤玲 <yangfengling@inttech.cn>
2021-01-04 11:19:30 +0800

    反转链表

---
+++ 腾讯/链表/反转链表.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+'''
+题目：反转链表
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre,cur = None,head
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
