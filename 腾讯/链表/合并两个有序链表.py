1fc1b7959e4134f2a5217557f052a4b58e279da8
杨凤玲 <yangfengling@inttech.cn>
2021-01-04 16:28:49 +0800

    合并两个有序链表

---
+++ 腾讯/链表/合并两个有序链表.py
@@ -0,0 +1,38 @@
+'''
+合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+示例：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        cur = res = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return res.next
+
+'''
+代码讲解网址：
+https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/solution/mian-shi-ti-25-he-bing-liang-ge-pai-xu-de-lian-b-2/
+'''
