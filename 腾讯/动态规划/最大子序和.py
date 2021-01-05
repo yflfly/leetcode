758e4f85e1f16094dfb68c7a00a13e24b41c38f9
杨凤玲 <yangfengling@inttech.cn>
2021-01-05 16:21:56 +0800

    最大子序和

---
+++ 腾讯/动态规划/最大子序和.py
@@ -0,0 +1,19 @@
+'''
+最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+
+输入: [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+'''
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0 for i in nums]
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+        return max(dp)
