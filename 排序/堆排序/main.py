1c6f197aaaa7166f4e1136609caa559e8eb2135d
杨凤玲 <yangfengling@inttech.cn>
2021-01-06 11:10:48 +0800

    堆排序

--- 排序/堆排序/main.py
+++ 排序/堆排序/main.py
@@ -27,6 +27,8 @@ def swap(nums, i, j):
 def heap_sort(nums):
     len_heap = len(nums) - 1
     len_mid_heap = int(len_heap / 2)
+    print('len_heap', len_heap)
+    print('len_mid_heap', len_mid_heap)
 
     for i in range(len_mid_heap):
         heap_adjust(nums, len_mid_heap - i, len_heap)
@@ -39,9 +41,26 @@ def heap_sort(nums):
 
 def main():
     L = deque([50, 16, 30, 10, 60, 90, 2, 80, 70])
+    print('初始', L)
     L.appendleft(0)
+    print('初始', L)
     print(heap_sort(L))
 
 
 if __name__ == '__main__':
     main()
+'''
+堆排序参考网址：
+https://blog.csdn.net/sxhelijian/article/details/50295637
+
+
+堆排序涉及到的概念：
+1、堆排序是利用 堆进行排序的
+2、堆是一种完全二叉树
+3、堆有两种类型: 大根堆 小根堆
+4、两种类型的概念如下：
+    1）大根堆：每个结点的值都大于或等于左右孩子结点
+    2）小根堆：每个结点的值都小于或等于左右孩子结点
+
+完全二叉树：是一种除了最后一层之外的其他每一层都被完全填充，并且所有结点都保持向左对齐的树
+'''
