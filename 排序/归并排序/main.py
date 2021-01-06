3e970d02c2be5945bfce56338bddca2b649cc595
杨凤玲 <yangfengling@inttech.cn>
2021-01-06 10:22:05 +0800

    归并排序

--- 排序/归并排序/main.py
+++ 排序/归并排序/main.py
@@ -2,15 +2,6 @@
 import time
 
 
-def merge_sort(nums):
-    if len(nums) <= 1:
-        return nums
-    middle = len(nums) // 2
-    left_num = nums[: middle]
-    right_num = nums[middle:]
-    return merge(merge_sort(left_num), merge_sort(right_num))
-
-
 def merge(left_num, right_num):
     len_left = len(left_num)
     len_right = len(right_num)
@@ -32,6 +23,15 @@ def merge(left_num, right_num):
     return result
 
 
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+    middle = len(nums) // 2
+    left_num = nums[: middle]
+    right_num = nums[middle:]
+    return merge(merge_sort(left_num), merge_sort(right_num))
+
+
 if __name__ == "__main__":
     start = time.time()
     #    for i in range(1000):
@@ -41,7 +41,20 @@ if __name__ == "__main__":
     print(result)
     print('Running time: %s Seconds' % (end - start))
 
+
+'''
+归并排序，是创建再归并操作上的一种有效的排序算法
+算法采用分治法的一个非常典型的应用，且各层分治递归可以同时进行
+归并排序思路简单，速度仅次于快速排序，为问丁排序算法，一般用于对总体无序，但是各子项相对有序的序列
+'''
+
 '''
 归并排序参考网址：
 https://zhuanlan.zhihu.com/p/124356219
+
+归并排序是用分治思想，分治模式在每一层递归上有三个步骤：
+分解（Divide）：将n个元素分成个含n/2个元素的子序列。
+解决（Conquer）：用合并排序法对两个子序列递归的排序。
+合并（Combine）：合并两个已排序的子序列已得到排序结果。
+
 '''
