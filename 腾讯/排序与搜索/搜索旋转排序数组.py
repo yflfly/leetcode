c9c7acc757167fd462bb5e5ffeb657daccfe8d47
杨凤玲 <yangfengling@inttech.cn>
2021-01-07 17:24:18 +0800

    搜索旋转排序数组

---
+++ 腾讯/排序与搜索/搜索旋转排序数组.py
@@ -0,0 +1,54 @@
+'''
+搜索旋转排序数组
+升序排列的整数数组 nums 在预先未知的某个点上进行了旋转（例如， [0,1,2,4,5,6,7] 经旋转后可能变为 [4,5,6,7,0,1,2] ）。
+请你在数组中搜索 target ，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+示例 1：
+输入：nums = [4,5,6,7,0,1,2], target = 0
+输出：4
+示例 2：
+输入：nums = [4,5,6,7,0,1,2], target = 3
+输出：-1
+示例 3：
+输入：nums = [1], target = 0
+输出：-1
+'''
+
+
+# 方法一
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
+
+
+# 方法二 二分搜索
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            mid = l+(r-l)//2
+            if nums[mid] == target:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+
+'''
+代码讲解网址：
+https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/sou-suo-xuan-zhuan-pai-xu-shu-zu-by-leetcode-solut/
+
+'''
