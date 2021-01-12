bdf003f5a470ed7c1f204cb768a5426d7136e69b
杨凤玲 <yangfengling@inttech.cn>
2021-01-12 08:59:05 +0800

    三数之和

--- 腾讯/数组与字符串/三数之和.py
+++ 腾讯/数组与字符串/三数之和.py
@@ -15,7 +15,6 @@
 '''
 
 
-# 数组遍历，会出现超时问题
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         if len(nums) < 3 or not nums:
@@ -48,11 +47,11 @@ class Solution:
 '''
 解题思路：
 标签：数组遍历
-首先对数组进行排序，排序后固定一个数 nums[i]nums[i]，再使用左右指针指向 nums[i]nums[i]后面的两端，数字分别为 nums[L]nums[L] 和 nums[R]nums[R]，计算三个数的和 sumsum 判断是否满足为 00，满足则添加进结果集
-如果 nums[i]nums[i]大于 00，则三数之和必然无法等于 00，结束循环
-如果 nums[i]nums[i] == nums[i-1]nums[i−1]，则说明该数字重复，会导致结果重复，所以应该跳过
-当 sumsum == 00 时，nums[L]nums[L] == nums[L+1]nums[L+1] 则会导致结果重复，应该跳过，L++L++
-当 sumsum == 00 时，nums[R]nums[R] == nums[R-1]nums[R−1] 则会导致结果重复，应该跳过，R--R−−
+首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，数字分别为 nums[L] 和 nums[R]，计算三个数的和sum判断是否满足为0，满足则添加进结果集
+如果 nums[i]大于 0，则三数之和必然无法等于0，结束循环
+如果 nums[i] == nums[i−1]，则说明该数字重复，会导致结果重复，所以应该跳过
+当 sum == 0 时，nums[L] == nums[L+1] 则会导致结果重复，应该跳过，L++
+当 sum == 0 时，nums[R] == nums[R−1] 则会导致结果重复，应该跳过，R--
 时间复杂度：O(n^2)
 '''
 
