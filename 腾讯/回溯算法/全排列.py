ef6de11e2f1fedb7edb7640c9e27c563ede574ca
杨凤玲 <yangfengling@inttech.cn>
2021-01-15 10:39:32 +0800

    回溯算法-全排列

---
+++ 腾讯/回溯算法/全排列.py
@@ -0,0 +1,30 @@
+'''
+全排列
+给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+相关标签  回溯算法
+'''
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(nums, path):
+            if not nums:
+                res.append(path)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], path + [nums[i]])
+        backtrack(nums, [])
+        return res
