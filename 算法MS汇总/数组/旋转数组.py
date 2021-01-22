ea2b538e43c8d6f9c7347f074414247c8e74f47e
杨凤玲 <yangfengling@inttech.cn>
2021-01-22 11:23:35 +0800

    算法面试汇总-旋转数组

---
+++ 算法MS汇总/数组/旋转数组.py
@@ -0,0 +1,49 @@
+'''
+旋转数组
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+进阶：
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+示例 1:
+输入: nums = [1,2,3,4,5,6,7], k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+输入：nums = [-1,-100,3,99], k = 2
+输出：[3,99,-1,-100]
+解释:
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+
+考查标签：数组
+'''
+
+
+# 该方法需要重新申请一个空间
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k == 0:
+            return nums
+        k = len(nums) % k
+        res = []
+        for each in nums[-k:]:
+            res.append(each)
+        for each in nums[:len(nums) - k]:
+            res.append(each)
+        return res
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            x = nums.pop()
+            nums.insert(0, x)
