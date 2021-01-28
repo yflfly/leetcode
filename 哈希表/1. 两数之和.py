f86984d53815a99edbdfa837d78186d3fb3fd938
杨凤玲 <yangfengling@inttech.cn>
2021-01-28 20:06:31 +0800

    两数之和-代码讲解

--- 哈希表/1. 两数之和.py
+++ 哈希表/1. 两数之和.py
@@ -11,7 +11,7 @@
 '''
 
 
-# 暴力方法
+# 方法一：暴力方法
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
@@ -21,7 +21,7 @@ class Solution:
         return []
 
 
-# 哈希表法
+# 方法二：哈希表法
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashtable = dict()
@@ -30,6 +30,13 @@ class Solution:
                 return [hashtable[target - num], i]
             hashtable[nums[i]] = i
         return []
+'''
+方法一的时间复杂度较高的原因是寻找target-x的时间复杂度过高。
+因此我们需要一种更优秀的方法，能够快速寻找数组中是否存在目标元素。如果存在，我们需要找出它的索引
+使用哈希表，可以将寻找target-x的时间复杂度从O(N)降低到O(1)
+我们创建一个哈希表，对于每一个x，我们首先查询到哈希表中是否存在target-x，然后将x插入到哈希表中，即可保证不会让x和自己匹配
+'''
+
 
 # 复现哈希表法 2021.1.28
 class Solution:
