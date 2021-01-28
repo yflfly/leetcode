91927195f16a0ebd6b5ea9d277483909bcb9aa53
杨凤玲 <yangfengling@inttech.cn>
2021-01-28 20:01:37 +0800

    1. 两数之和

--- 哈希表/1. 两数之和.py
+++ 哈希表/1. 两数之和.py
@@ -30,3 +30,13 @@ class Solution:
                 return [hashtable[target - num], i]
             hashtable[nums[i]] = i
         return []
+
+# 复现哈希表法 2021.1.28
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict1 = {}
+        for i in range(len(nums)):
+            if target-nums[i] in dict1:
+                return [dict1[target-nums[i]],i]
+            dict1[nums[i]] = i
+        return []
