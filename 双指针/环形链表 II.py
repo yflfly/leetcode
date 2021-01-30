c097ee2ab9c0337ac8045811644aa14af758a1a4
杨凤玲 <yangfengling@inttech.cn>
2021-01-30 14:48:47 +0800

    环形链表-快慢指针

---
+++ 双指针/环形链表 II.py
@@ -0,0 +1,69 @@
+'''
+环形链表 II
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+说明：不允许修改给定的链表。
+进阶：
+你是否可以使用 O(1) 空间解决此题？
+
+示例 1：
+输入：head = [3,2,0,-4], pos = 1
+输出：返回索引为 1 的链表节点
+解释：链表中有一个环，其尾部连接到第二个节点。
+示例 2：
+输入：head = [1,2], pos = 0
+输出：返回索引为 0 的链表节点
+解释：链表中有一个环，其尾部连接到第一个节点。
+示例 3：
+输入：head = [1], pos = -1
+输出：返回 null
+解释：链表中没有环。
+ 
+提示：
+
+链表中节点的数目范围在范围 [0, 104] 内
+-105 <= Node.val <= 105
+pos 的值为 -1 或者链表中的一个有效索引
+
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return None
+        dict1 = {}
+        tmp = head
+        while tmp:
+            if tmp not in dict1:
+                dict1[tmp] = 1
+            else:
+                return tmp
+            tmp = tmp.next
+        return None
+
+
+# 快慢指针
+'''
+先进行第一轮的查找，再进行第二轮的查找，第二轮查找相遇的节点即为环的入口节点
+'''
+class Solution(object):
+    def detectCycle(self, head):
+        fast, slow = head, head
+        while True:
+            if not (fast and fast.next):
+                return
+            fast, slow = fast.next.next, slow.next
+            if fast == slow:
+                break
+        fast = head
+        while fast != slow:
+            fast, slow = fast.next, slow.next
+        return fast
