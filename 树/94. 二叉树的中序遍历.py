ca01b7016880b81b1823dd45fa9a14d0bd6b8fbe
杨凤玲 <yangfengling@inttech.cn>
2021-02-02 17:18:02 +0800

    94. 二叉树的中序遍历

--- 树/94. 二叉树的中序遍历.py
+++ 树/94. 二叉树的中序遍历.py
@@ -62,9 +62,13 @@ class Solution:
         res = []
         stack = []
         while stack or root:
+            # 不断往左子树方向走，每走一次就将当前节点保存到栈中
+            # 这是模拟递归的调用
             if root:
                 stack.append(root)
                 root = root.left
+            # 当前节点为空，说明左边走到头了，从栈中弹出节点并保存
+            # 然后转向右边节点，继续上面整个过程
             else:
                 tmp = stack.pop()
                 res.append(tmp.val)
@@ -81,3 +85,4 @@ class Solution:
 https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/dong-hua-yan-shi-94-er-cha-shu-de-zhong-xu-bian-li/
 
 '''
+
