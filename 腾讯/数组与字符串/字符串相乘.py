66f4e205def5b74ba7379f242039a9fe62ae8b91
杨凤玲 <yangfengling@inttech.cn>
2021-03-13 22:31:14 +0800

    合并两个有序数组

--- 腾讯/数组与字符串/字符串相乘.py
+++ 腾讯/数组与字符串/字符串相乘.py
@@ -1,20 +1,17 @@
 '''
 字符串相乘
 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
-
 示例 1:
-
 输入: num1 = "2", num2 = "3"
 输出: "6"
 示例 2:
-
 输入: num1 = "123", num2 = "456"
 输出: "56088"
 
 考查标签：数学、字符串
 '''
 
-
+# 方法一
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         return str(int(num1) * int(num2))
