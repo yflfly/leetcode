25f1b7ef3fbeb1fe75d8cdd0a9fc8c6ee8d7a2ab
杨凤玲 <yangfengling@inttech.cn>
2021-03-15 20:06:30 +0800

    最长公共前缀

--- 腾讯/数组与字符串/最长公共前缀.py
+++ 腾讯/数组与字符串/最长公共前缀.py
@@ -41,3 +41,25 @@ class Solution:
 代码讲解网址：
 https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
 '''
+
+
+# 方法二：纵向扫描
+'''
+方法一是横向扫描，依次遍历每个字符串，更新最长公共前缀。
+另一种方法是纵向扫描。纵向扫描时，从前往后遍历所有字符串的每一列，比较相同列上的字符是否相同，
+如果相同则继续对下一列进行比较，如果不相同则当前列不再属于公共前缀，当前列之前的部分为最长公共前缀。
+
+'''
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:  # 若列表为空 则返回空，即输入不存在公共前缀
+            return ""
+        len_1 = len(strs[0])
+        len_all = len(strs)
+        for i in range(len_1):
+            str1 = strs[0][i]
+            for j in range(1, len_all):
+                if i == len(strs[j]) or strs[j][i] != str1:
+                    return strs[0][:i]
+        return strs[0]
+
