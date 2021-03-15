506127c0c9570c16f298add70b63832ff7191cf1
杨凤玲 <yangfengling@inttech.cn>
2021-03-15 11:11:44 +0800

    合并两个有序数组

--- 腾讯/数组与字符串/合并两个有序数组.py
+++ 腾讯/数组与字符串/合并两个有序数组.py
@@ -1,7 +1,8 @@
 '''
 合并两个有序数组
 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
-初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 有足够的空间（空间大小等于 m + n）来保存 nums2 中的元素。
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+你可以假设 nums1 有足够的空间（空间大小等于 m + n）来保存 nums2 中的元素。
 示例 1：
 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 输出：[1,2,2,3,5,6]
@@ -19,7 +20,7 @@
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m: int, nums2, n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -39,7 +40,7 @@ class Solution:
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m: int, nums2, n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -60,3 +61,26 @@ class Solution:
         if p1 < m:
             for each in nums1_copy[p1:]:
                 nums1.append(each)
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+nums1_copy = nums1[:m]
+nums1[:] = []
+p1, p2 = 0, 0
+while p1 < m and p2 < n:
+    if nums1_copy[p1] < nums2[p2]:
+        nums1.append(nums1_copy[p1])
+        p1 += 1
+    else:
+        nums1.append(nums2[p2])
+        p2 += 1
+if p1 < m:
+    for each in nums1_copy[p1:]:
+        nums1.append(each)
+if p2 < n:
+    for each in nums2[p2:]:
+        nums1.append(each)
+print(nums1)
