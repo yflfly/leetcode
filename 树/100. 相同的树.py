49917d3ab014d09d642b5b7e6aef7845f0197bc7
杨凤玲 <yangfengling@inttech.cn>
2021-03-16 15:27:53 +0800

    100. 相同的树

--- 树/100. 相同的树.py
+++ 树/100. 相同的树.py
@@ -1,4 +1,6 @@
 # coding:utf-8
+import collections
+
 '''
 100. 相同的树
 给定两个二叉树，编写一个函数来检验它们是否相同。
@@ -52,11 +54,15 @@ class Solution:
         if not self.isSameTree(p.right, q.right):
             return False
         return True
+
+
 # 方法一：深度优先搜索
 '''
 如果两个二叉树都为空，则两个二叉树相同。如果两个二叉树中有且只有一个为空，则两个二叉树一定不相同。
 如果两个二叉树都不为空，那么首先判断它们的根节点的值是否相同，若不相同则两个二叉树一定不同，若相同，再分别判断两个二叉树的左子树是否相同以及右子树是否相同。这是一个递归的过程，因此可以使用深度优先搜索，递归地判断两个二叉树是否相同。
 '''
+
+
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
@@ -67,3 +73,35 @@ class Solution:
             return False
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+# 方法二：广度优先搜索
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        queue1 = collections.deque([p])
+        queue2 = collections.deque([q])
+        while queue1 and queue2:
+            node1 = queue1.popleft()
+            node2 = queue2.popleft()
+            # 如果两个节点的值不相同则两个二叉树一定不同
+            if node1.val != node2.val:
+                return False
+            left1, right1 = node1.left, node1.right
+            left2, right2 = node2.left, node2.right
+            if (not left1) ^ (not left2):  # 两者相同 则为true 可以理解为异或
+                return False
+            if (not right1) ^ (not right2):
+                return False
+            if left1:
+                queue1.append(left1)
+            if right1:
+                queue1.append(right1)
+            if left2:
+                queue2.append(left2)
+            if right2:
+                queue2.append(right2)
+        return not queue1 and not queue2
