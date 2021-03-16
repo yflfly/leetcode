8bc20ec9bcfa450f25132d0247b8647133bbcaf6
杨凤玲 <yangfengling@inttech.cn>
2021-03-16 15:15:24 +0800

    100. 相同的树

--- 树/100. 相同的树.py
+++ 树/100. 相同的树.py
@@ -52,3 +52,18 @@ class Solution:
         if not self.isSameTree(p.right, q.right):
             return False
         return True
+# 方法一：深度优先搜索
+'''
+如果两个二叉树都为空，则两个二叉树相同。如果两个二叉树中有且只有一个为空，则两个二叉树一定不相同。
+如果两个二叉树都不为空，那么首先判断它们的根节点的值是否相同，若不相同则两个二叉树一定不同，若相同，再分别判断两个二叉树的左子树是否相同以及右子树是否相同。这是一个递归的过程，因此可以使用深度优先搜索，递归地判断两个二叉树是否相同。
+'''
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
