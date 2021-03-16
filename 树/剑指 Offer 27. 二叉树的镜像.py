f35eac1dd1487a4c1b6750e1783e774623c908e6
杨凤玲 <yangfengling@inttech.cn>
2021-03-16 16:47:37 +0800

    剑指 Offer 27. 二叉树的镜像

--- 树/剑指 Offer 27. 二叉树的镜像.py
+++ 树/剑指 Offer 27. 二叉树的镜像.py
@@ -2,41 +2,32 @@
 '''
 剑指 Offer 27. 二叉树的镜像
 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
-
 例如输入：
-
      4
    /   \
   2     7
  / \   / \
 1   3 6   9
 镜像输出：
-
      4
    /   \
   7     2
  / \   / \
 9   6 3   1
-
- 
-
 示例 1：
-
 输入：root = [4,2,7,1,3,6,9]
 输出：[4,7,2,9,6,3,1]
- 
 限制：
 0 <= 节点个数 <= 1000
-
 '''
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 # 方法一：递归法
 class Solution:
     def mirrorTree(self, root: TreeNode) -> TreeNode:
