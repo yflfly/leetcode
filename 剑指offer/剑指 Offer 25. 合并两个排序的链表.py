2f0e040011c3ebb2ba4ff4299d5547d8c5d19e91
杨凤玲 <yangfengling@inttech.cn>
2021-03-17 09:18:49 +0800

    剑指 Offer 25. 合并两个排序的链表

--- 剑指offer/剑指 Offer 25. 合并两个排序的链表.py
+++ 剑指offer/剑指 Offer 25. 合并两个排序的链表.py
@@ -2,9 +2,7 @@
 '''
 剑指 Offer 25. 合并两个排序的链表
 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
-
 示例1：
-
 输入：1->2->4, 1->3->4
 输出：1->1->2->3->4->4
 限制：
@@ -13,10 +11,10 @@
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
@@ -35,3 +33,8 @@ class Solution:
             cur = cur.next
         cur.next = l1 if l1 else l2
         return res.next
+'''
+复杂度分析：
+时间复杂度O(M+N)：M,N分别为链表l_1, l_2的长度，合并操作需遍历两链表。
+空间复杂度O(1)： 节点引用head,cur使用常数大小的额外空间。
+'''
