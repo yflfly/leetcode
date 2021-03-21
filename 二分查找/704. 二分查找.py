6934a0b762edcf1fbf6ee23183221946e5297d18
杨凤玲 <yangfengling@inttech.cn>
2021-03-21 14:25:04 +0800

    704. 二分查找

--- 二分查找/704. 二分查找.py
+++ 二分查找/704. 二分查找.py
@@ -3,7 +3,6 @@
 704. 二分查找
 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，
 如果目标值存在返回下标，否则返回 -1。
-
 示例 1:
 输入: nums = [-1,0,3,5,9,12], target = 9
 输出: 4
@@ -26,4 +25,8 @@ class Solution:
             else:
                 right = middle-1
         return -1
-
+'''
+复杂度分析
+时间复杂度：O(logN)
+空间复杂度：O(1)
+'''
