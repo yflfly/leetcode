dfba8bea9f26cd903a6e46af592f885fe9d803e7
杨凤玲 <yangfengling@inttech.cn>
2021-03-23 09:10:53 +0800

    257. 二叉树的所有路径

--- 树/257. 二叉树的所有路径.py
+++ 树/257. 二叉树的所有路径.py
@@ -29,12 +29,22 @@ class Solution:
         def get_path(root, path):
             if root:
                 path += str(root.val)
+                # 当前结点是叶子结点
                 if not root.left and not root.right:
-                    paths.append(path)
+                    paths.append(path)  # 把路径加入到答案中
                 else:
-                    path += '->'
+                    path += '->'  # 当前结点不是叶子结点，继续递归遍历
                     get_path(root.left, path)
                     get_path(root.right, path)
+
         paths = []
         get_path(root, '')
         return paths
+'''
+方法一：深度优先搜索
+思路与算法
+最直观的方法是使用深度优先搜索。在深度优先搜索遍历二叉树时，我们需要考虑当前的节点以及它的孩子节点。
+如果当前节点不是叶子节点，则在当前的路径末尾添加该节点，并继续递归遍历该节点的每一个孩子节点。
+如果当前节点是叶子节点，则在当前路径末尾添加该节点后我们就得到了一条从根节点到叶子节点的路径，将该路径加入到答案即可。
+如此，当遍历完整棵二叉树以后我们就得到了所有从根节点到叶子节点的路径。当然，深度优先搜索也可以使用非递归的方式实现，这里不再赘述。
+'''
