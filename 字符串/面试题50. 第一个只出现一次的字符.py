b76ba6517f8444e443f9feb9cd0d92700d887315
杨凤玲 <yangfengling@inttech.cn>
2021-04-01 13:44:27 +0800

    第一个只出现一次的字符

--- 字符串/面试题50. 第一个只出现一次的字符.py
+++ 字符串/面试题50. 第一个只出现一次的字符.py
@@ -7,6 +7,9 @@ s = "abaccdeff"
 s = ""
 返回 " "
 '''
+
+
+# 方法一：使用字典存储频数
 class Solution:
     def firstUniqChar(self, s: str) -> str:
         d1 = {}
@@ -19,3 +22,10 @@ class Solution:
             if d1[key] == 1:
                 return key
         return ' '
+
+
+'''
+我们可以对字符串进行两次遍历。
+在第一次遍历时，我们使用哈希映射统计出字符串中每个字符出现的次数。
+在第二次遍历时，我们只要遍历到了一个只出现一次的字符，那么就返回该字符，否则在遍历结束后返回空格。
+'''
