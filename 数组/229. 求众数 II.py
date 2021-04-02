d3f654f6f316ce2e1eba9a5794fbe0d06c2c4a3f
杨凤玲 <yangfengling@inttech.cn>
2021-04-02 10:27:00 +0800

    229. 求众数 II

--- 数组/229. 求众数 II.py
+++ 数组/229. 求众数 II.py
@@ -29,3 +29,52 @@ class Solution:
             if dict1[key] > count:
                 res.append(key)
         return res
+
+
+# 摩尔投票法
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        a, b, count_a, count_b = 0, 0, 0, 0  # 设定1号众数和2号众数
+        res = []
+
+        for i in nums:
+            if a == i:  # 频数统计的优先顺序要大于频数为0的判断
+                count_a += 1
+                continue
+            if b == i:
+                count_b += 1
+                continue
+            if count_a == 0:
+                a = i
+                count_a = 1
+                continue
+            if count_b == 0:
+                b = i
+                count_b = 1
+                continue
+            count_a -= 1
+            count_b -= 1
+
+        count_a, count_b = 0, 0  # 重置计数器
+        for j in nums:  # 再检验
+            if j == a:
+                count_a += 1
+            elif j == b:
+                count_b += 1
+        if count_a > len(nums) / 3:
+            res.append(a)
+        if count_b > len(nums) / 3:
+            res.append(b)
+        return res
+'''
+复杂度分析
+时间复杂度：O(N)
+空间复杂度：O(1)
+'''
+
+'''
+如果至多选一个代表，那他的票数至少要超过一半（⌊ 1/2 ⌋）的票数；
+如果至多选两个代表，那他们的票数至少要超过 ⌊ 1/3 ⌋ 的票数；
+如果至多选m个代表，那他们的票数至少要超过 ⌊ 1/(m+1) ⌋ 的票数。
+所以以后碰到这样的问题，而且要求达到线性的时间复杂度以及常量级的空间复杂度，直接套上摩尔投票法。
+'''
