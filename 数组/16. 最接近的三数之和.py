e1ed2c258cde638b132639fe69d59c030c5f2b21
杨凤玲 <yangfengling@inttech.cn>
2021-04-02 09:08:45 +0800

    最接近的三数之和

---
+++ 数组/16. 最接近的三数之和.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+'''
+最接近的三数之和
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。
+返回这三个数的和。假定每组输入只存在唯一答案。
+示例：
+输入：nums = [-1,2,1,-4], target = 1
+输出：2
+解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+'''
+
+# 排序+双指针
+class Solution:
+    def threeSumClosest(self, nums, target: int) -> int:
+        if len(nums) < 3 or not nums:
+            return []
+        nums_sorted = sorted(nums)  # 先从小到大排序
+        n = len(nums)
+        res = nums_sorted[0] + nums_sorted[1] + nums_sorted[2]
+        for i in range(n):
+            left = i + 1
+            right = n - 1
+            while left < right:
+                sum1 = nums_sorted[i] + nums_sorted[left] + nums_sorted[right]
+                if abs(target - sum1) < abs(target - res):
+                    res = sum1
+                if sum1 > target:
+                    right -= 1
+                elif sum1 < target:
+                    left += 1
+                else:
+                    return res
+        return res
+'''
+复杂度的分析：
+时间复杂度：O(n^2)，n表示的是数组的长度
+空间复杂度：O(n)
+'''
