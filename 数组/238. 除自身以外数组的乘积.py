c415bd2364666c6e5e6d188ee1c0fd3f8a77f6e9
杨凤玲 <yangfengling@inttech.cn>
2021-04-05 21:57:50 +0800

    238. 除自身以外数组的乘积

--- 数组/238. 除自身以外数组的乘积.py
+++ 数组/238. 除自身以外数组的乘积.py
@@ -23,11 +23,11 @@ class Solution:
         for i in range(1, length):
             L[i] = nums[i - 1] * L[i - 1]
         # R[i] 为索引 i 右侧所有元素的乘积
-        # 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        # 对于索引为'length-1'的元素，因为右侧没有元素，所以 R[length-1] = 1
         R[length - 1] = 1
         for i in reversed(range(length - 1)):
             R[i] = nums[i + 1] * R[i + 1]
-        # 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        # 对于索引i，除nums[i]之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
         for i in range(length):
             answer[i] = L[i] * R[i]
         return answer
