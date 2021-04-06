5af51d9ab3a909700d42157714ddb60311971867
杨凤玲 <yangfengling@inttech.cn>
2021-04-06 16:51:44 +0800

    120. 三角形最小路径和

--- 动态规划/120. 三角形最小路径和.py
+++ 动态规划/120. 三角形最小路径和.py
@@ -19,20 +19,21 @@
 class Solution:
     def minimumTotal(self, triangle) -> int:
         n = len(triangle)
-        f = [[0] * n for _ in range(n)]
-        f[0][0] = triangle[0][0]
+        dp = [[0] * n for _ in range(n)]
+        dp[0][0] = triangle[0][0]
         for i in range(1, n):
-            f[i][0] = f[i - 1][0] + triangle[i][0]
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
             for j in range(1, i):
-                f[i][j] = min(f[i - 1][j - 1], f[i - 1][j]) + triangle[i][j]
-            f[i][i] = f[i - 1][i - 1] + triangle[i][i]
-        return min(f[n - 1])
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+        return min(dp[n - 1])
 
 
 '''
 背景：
-本题是一道非常经典且历史悠久的动态规划题，其作为算法题出现，最早可以追溯到 1994 年的 IOI（国际信息学奥林匹克竞赛）的 The Triangle。时光飞逝，经过 20 多年的沉淀，往日的国际竞赛题如今已经变成了动态规划的入门必做题，不断督促着我们学习和巩固算法。
-
+本题是一道非常经典且历史悠久的动态规划题，其作为算法题出现，最早可以追溯到 1994 年的 IOI（国际信息学奥林匹克竞赛）
+的 The Triangle。时光飞逝，经过 20 多年的沉淀，往日的国际竞赛题如今已经变成了动态规划的入门必做题，
+不断督促着我们学习和巩固算法。
 '''
 
 triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
