129536db1b01025598180a509ea383d13ad6880b
杨凤玲 <yangfengling@inttech.cn>
2021-04-06 15:58:04 +0800

    70. 爬楼梯

--- 动态规划/70. 爬楼梯.py
+++ 动态规划/70. 爬楼梯.py
@@ -20,15 +20,16 @@
 
 '''
 
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
-        dp = [0 for i in range(n+1)]
+        dp = [0 for i in range(n + 1)]
         dp[1] = 1
         dp[2] = 2
         for i in range(3, n + 1):
-            dp[i] = dp[i-1]+dp[i-2]
+            dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]
