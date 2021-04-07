f96d411f2a2c5c0ca232fa07fc983f7299b5cff5
杨凤玲 <yangfengling@inttech.cn>
2021-04-07 11:24:09 +0800

    122. 买卖股票的最佳时机 II

--- 动态规划/122. 买卖股票的最佳时机 II.py
+++ 动态规划/122. 买卖股票的最佳时机 II.py
@@ -1,5 +1,5 @@
 '''
-买卖股票的最佳时机 II
+122. 买卖股票的最佳时机 II
 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
@@ -34,3 +34,25 @@ class Solution:
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
         return dp[-1][0]
+
+
+'''
+考虑到不能参与多笔交易，因此每天交易结束后只可能存在手里有一支股票或者没有股票的状态
+dp[i][0]：表示第i天交易完成后手里没有股票的最大利润
+dp[i][1]：表示第i天交易完成后手里持有一支股票的最大利润(i从0开始)
+'''
+
+# 使用2个一维数组表示dp
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        dp_0 = [0 for i in prices]  # 第i天手里没有股票
+        dp_1 = [0 for i in prices]  # 第i天手里有股票
+        dp_1[0] = -prices[0]
+        for i in range(1, len(prices)):
+            dp_0[i] = max(dp_0[i - 1], dp_1[i - 1] + prices[i])
+            dp_1[i] = max(dp_1[i - 1], dp_0[i - 1] - prices[i])
+        return dp_0[-1]
