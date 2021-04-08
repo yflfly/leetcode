5710d9ce36cc98d61cfb155ce58d795ad3cf3ff3
杨凤玲 <yangfengling@inttech.cn>
2021-04-08 14:15:03 +0800

    434. 字符串中的单词数

--- 2021/434. 字符串中的单词数.py
+++ 2021/434. 字符串中的单词数.py
@@ -10,3 +10,11 @@
 class Solution:
     def countSegments(self, s: str) -> int:
         return len(s.split())
+
+class Solution:
+    def countSegments(self, s):
+        segment_count = 0
+        for i in range(len(s)):
+            if (i == 0 or s[i-1] == ' ') and s[i] != ' ':
+                segment_count += 1
+        return segment_count
