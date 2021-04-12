95fe6e18180ba4eb2c30e78edd8a1a27781ae90d
杨凤玲 <yangfengling@inttech.cn>
2021-04-12 13:59:04 +0800

    python基础知识

---
+++ python-MST/随机列表中的元素.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+from random import shuffle
+
+x = ['Keep', 'The', 'Blue', 'Flag', 'Flying', 'High']
+shuffle(x)
+print(x)
+
+'''
+输出结果如下所示：
+['Flag', 'High', 'Keep', 'Blue', 'Flying', 'The']
+'''
