ecb09cf7a44927a29fa6dad549754fa145a6bd78
杨凤玲 <yangfengling@inttech.cn>
2021-04-13 14:40:24 +0800

    572. 另一个树的子树

--- 剑指offer/剑指 Offer 40. 最小的k个数.py
+++ 剑指offer/剑指 Offer 40. 最小的k个数.py
@@ -2,19 +2,46 @@
 '''
 剑指 Offer 40. 最小的k个数
 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
-
 示例 1：
-
 输入：arr = [3,2,1], k = 2
 输出：[1,2] 或者 [2,1]
 示例 2：
-
 输入：arr = [0,1,2,1], k = 1
 输出：[0]
 
 '''
 
+
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         arr.sort()
         return arr[:k]
+
+
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        n = len(arr)
+        self.buildHeap(arr, n)
+        for i in range(n - 1, -1, -1):
+            self.swap(arr, i, 0)
+            self.heapify(arr, i, 0)
+        return arr[:k]
+
+    def swap(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    def buildHeap(self, arr, n):
+        for i in range((n - 2) // 2, -1, -1):
+            self.heapify(arr, n, i)
+
+    def heapify(self, arr, n, i):
+        c1 = 2 * i + 1
+        c2 = 2 * i + 2
+        max = i
+        if c1 < n and arr[c1] > arr[max]:
+            max = c1
+        if c2 < n and arr[c2] > arr[max]:
+            max = c2
+        if max != i:
+            self.swap(arr, max, i)
+            self.heapify(arr, n, max)
---
+++ 树/572. 另一个树的子树.py
@@ -0,0 +1,56 @@
+'''
+572. 另一个树的子树
+给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+示例 1:
+给定的树 s:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 t：
+   4
+  / \
+ 1   2
+返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+示例 2:
+给定的树 s：
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 false。
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s and not t:  # 均为空   递归出口
+            return True
+        if not s or not t:  # 有一个非空
+            return False
+        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, s, t):
+        if not s and not t:  # 递归出口
+            return True
+        if not s or not t:
+            return False
+        return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
