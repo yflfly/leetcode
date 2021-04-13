c55853a0f2a0363f788b396470556cf185578cf6
杨凤玲 <yangfengling@inttech.cn>
2021-04-13 16:18:16 +0800

    2. 两数相加

--- 双指针/2. 两数相加.py
+++ 双指针/2. 两数相加.py
@@ -16,7 +16,11 @@
 相加得到结果；
 将结果转换为一个链表。
 '''
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
