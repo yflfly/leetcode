ec6ed82cd4176a61ac40c782104bdae0bdad2714
杨凤玲 <yangfengling@inttech.cn>
2021-04-13 15:10:26 +0800

    226. 翻转二叉树

---
+++ 树/226. 翻转二叉树.py
@@ -0,0 +1,33 @@
+'''
+226. 翻转二叉树
+翻转一棵二叉树。
+示例：
+输入：
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
