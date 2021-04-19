ea8a28ad39e48aea1b6194c8f879e95da969e723
杨凤玲 <yangfengling@inttech.cn>
2021-04-19 10:38:14 +0800

    287. 寻找重复数

---
+++ 数组/665. 非递减数列.py
@@ -0,0 +1,28 @@
+'''
+665. 非递减数列
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+我们是这样定义一个非递减数列的： 对于数组中任意的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+示例 1:
+输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+示例 2:
+输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+'''
+
+
+# [3,4,2,3]
+class Solution(object):
+    def checkPossibility(self, nums):
+        N = len(nums)
+        count = 0
+        for i in range(1, N):
+            if nums[i] < nums[i - 1]:
+                count += 1
+                if i == 1 or nums[i] >= nums[i - 2]:
+                    nums[i - 1] = nums[i]
+                else:
+                    nums[i] = nums[i - 1]
+        return count <= 1
