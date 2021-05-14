0f91403f829348ad065d4794b1216e6b3c351c63
杨凤玲 <yangfengling@inttech.cn>
2021-05-14 15:55:44 +0800

    1684. 统计一致字符串的数目

---
+++ 字符串/1684. 统计一致字符串的数目.py
@@ -0,0 +1,33 @@
+'''
+1684. 统计一致字符串的数目
+给你一个由不同字符组成的字符串 allowed 和一个字符串数组 words 。如果一个字符串的每一个字符都在 allowed 中，就称这个字符串是 一致字符串 。
+请你返回 words 数组中 一致字符串 的数目。
+示例 1：
+输入：allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+输出：2
+解释：字符串 "aaab" 和 "baa" 都是一致字符串，因为它们只包含字符 'a' 和 'b' 。
+示例 2：
+输入：allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+输出：7
+解释：所有字符串都是一致的。
+示例 3：
+输入：allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+输出：4
+解释：字符串 "cc"，"acd"，"ac" 和 "d" 是一致字符串。
+
+考查点：字符串
+'''
+
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        list1 = list(allowed)
+        res = 0
+        for word in words:
+            flag = 0
+            for each in list(word):
+                if each not in list1:
+                    flag += 1
+            if flag == 0:
+                res += 1
+        return res
