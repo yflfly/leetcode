8ea1510ed4e026b3d1a899dd81ce5b02cacc999e
yflfly <1091283542@qq.com>
2022-07-03 16:29:33 +0800

    提交

--- 动态规划/509. 斐波那契数.py
+++ 动态规划/509. 斐波那契数.py
@@ -26,8 +26,27 @@ class Solution:
         if n <= 2:
             return 1
         dp = [0 for _ in range(n)]
-        dp[0] = 1
+        dp[0] = 0
         dp[1] = 1
         for i in range(2, n):
             dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]
+
+
+'''
+java:
+class Solution {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q; 
+            q = r; 
+            r = p + q;
+        }
+        return r;
+    }
+}
+'''
