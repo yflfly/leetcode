7d65361daf4096769d23528de34de45a18f72f86
yflfly <1091283542@qq.com>
2022-09-27 21:47:09 +0800

    存在 重复元素

--- 2020-start/48.py
+++ 2020-start/48.py
@@ -11,6 +11,8 @@
 输入: [4,1,2,1,2]
 输出: 4
 '''
+
+
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -22,3 +24,40 @@ class Solution(object):
             if nums[i] != nums[i + 1]:
                 return nums[i]
         return nums[-1]
+
+
+# 该方法用到了额外的空间，不符合题目的要求
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict_1 = {}
+        for each in nums:
+            if each in dict_1:
+                dict_1[each] += 1
+            else:
+                dict_1[each] = 1
+        for each in dict_1.keys():
+            if dict_1[each] == 1:
+                return each
+# 异或处理
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
+'''
+复杂度分析
+时间复杂度：O(n)，其中 n 是数组长度。只需要对数组遍历一次。
+空间复杂度：O(1)。
+'''
+'''
+1)相等两数x, y的异或(x^y)为0
+2)lambda匿名函数可以传入其的变量进行计算，
+如func = lambda x,y:x+y定义了一个进行求和的匿名函数
+res = func(1,2) -> res = 3
+3)reduce(func, iterable)函数可对可迭代对象iterable的每一个元素
+送入func进行累积运算，例如：reduce(lambda x,y:x+y, [1,2,3]) -> ((1+2)+3)=6
+
+'''
+
+'''
+leetcode链接：
+https://leetcode.cn/problems/single-number/solution/zhi-chu-xian-yi-ci-de-shu-zi-by-leetcode-solution/
+'''
